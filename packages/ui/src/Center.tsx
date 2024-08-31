bf7ece3f7ecfef900c5b2a8008067fb29f2bf81f
MohammedYaseenRon <yaseenron3@gmail.com>
2024-08-31 15:56:41 +0530

    updataed packages

---
+++ packages/ui/src/Center.tsx
@@ -0,0 +1,10 @@
+
+import React from "react"
+
+export const Center=({ children }: {children: React.ReactNode}) => {
+    return <div className="flex justify-center flex-col h-full">
+        <div className="flex justify-center">
+            {children}
+        </div>
+    </div>
+}
