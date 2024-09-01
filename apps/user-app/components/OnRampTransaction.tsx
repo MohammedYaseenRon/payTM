e1983aa43b922cae67bbe5d918b86e2d038ca37a
MohammedYaseenRon <yaseenron3@gmail.com>
2024-09-01 12:19:33 +0530

    added data in transafer page

--- apps/user-app/components/OnRampTransaction.tsx
+++ apps/user-app/components/OnRampTransaction.tsx
@@ -1,9 +1,40 @@
+import { Card } from "@repo/ui/card";
 
 
-export const OnRampTransaction = () => {
-    return (
-        <div>
+export const OnRampTransactions = ({
+    transactions
+}: {
+    transactions: {
+        time: Date,
+        amount: number,
+        status: string,
+        provider: string
+    }[]
+}) => {
+     if(!transactions.length) {
+        return <Card title="Recent Transactions">
+            <div className="pt-8 pb-8 text-center">
+                No Recent transactions
+            </div>
+        </Card>
+    }
+    return <Card title="Recent Transactions">
+        <div className="pt-2">
+            {transactions.map(t => <div className="flex justify-between">
+                <div>
+                    <div className="text-sm">
+                        Received INR
+                    </div>
+                    <div className="text-slate-600 text-xs">
+                        {t.time.toDateString()}
+                    </div>
+                </div>
+
+                <div className="flex flex-col justify-center">
+                    + Rs {t.amount / 100 }
+                </div>
             
+            </div>)}
         </div>
-    )
+    </Card>   
 }
